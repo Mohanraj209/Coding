fec9ee7b3b4a50a74b7879d8f603ab4c754c777d
Mohanraj209 <mohanraj1715@gmail.com>
2022-01-13 00:51:34 +0530

    Program to Add two random dice number and print the result

---
+++ Dice_Number SUM.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+value=$(( $RANDOM + $RANDOM ))
+echo "$value"
---
+++ Dice_Number_sum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+value1=$(( $RANDOM % 2 ))
+value2=$(( $RANDOM % 2 ))
+  sum=$(( $value1 + $value2 ))
+echo "$sum"
+
