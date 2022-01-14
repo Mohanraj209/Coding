5b55375d2df080605903e1d0f480b2bad38232ec
Mohanraj209 <97426812+Mohanraj209@users.noreply.github.com>
2022-01-14 22:03:48 +0530

    Delete area.sh

--- area.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo 'Enter the width of the rectangle'
-read W
-echo 'Enter the length of the rectangle'
-read L
-Area=`echo "$W $L" | awk '{area=$1*$2*0.3048; print area}'`
-echo "$Area meters"
