39285e671e7f005b35a38bb7f31554c43a082a25
Mohanraj209 <mohanraj1715@gmail.com>
2022-01-19 09:32:52 +0530

    Program to check if leap year or not

---
+++ single_num.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+read num1;num2;num3;num4;num5;num6;num7;num8;num9;num0;
+echo "One:$num1 , Two:$num2 , Three:$num3 , Four:$num4 , Five:$num5 , Six:$num6 , Seven:$num7 , Eight:$num8 , Nine:$num9 , Zero:$num0"
